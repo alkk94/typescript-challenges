340a31eb19bd4c346dd05c144943b6d59cdd9236
alk <adrianlukasik994@gmail.com>
2023-06-16 21:38:08 +0200

    Updated constraints.ts and media.ts

--- src/constraints/constraints.ts
+++ src/constraints/constraints.ts
@@ -9,15 +9,16 @@
 * Hint: https://www.typescriptlang.org/docs/handbook/generics.html
 */
 
-interface Book {
+interface Info {
     id: number;
     name: string;
+}
+
+interface Book extends Info {
     author: string;
 }
 
-interface Video {
-    id: number;
-    name: string;
+interface Video extends Info {
     length: number;
 }
 
@@ -36,7 +37,7 @@ const videos: Video[] = [
     { id: 5, name: 'Poznaj TypeScript', length: 22 }
 ]
 
-function processItems<T>(items: T[]): T[] {
+function processItems<T extends Info>(items: T[]): Info[] {
     return items
         .filter(({ id }) => id > 2)
         .map(({ id, name }) => ({ id, name }));
