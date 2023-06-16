a64c94bbb9caf23485007eee41b31d8c0e656e1e
alk <adrianlukasik994@gmail.com>
2023-06-16 22:02:05 +0200

    Updated shortcuts.ts

--- src/shortcuts/shortcuts.ts
+++ src/shortcuts/shortcuts.ts
@@ -11,7 +11,7 @@
 
 class Bag {
     constructor(
-        items: string[]
+        protected readonly items: string[]
     ) { }
 
     lookInside() {
@@ -21,8 +21,8 @@ class Bag {
 
 class BrandedBag extends Bag {
     constructor(
-        logo: string,
-        brand: string,
+        readonly logo: string,
+        private readonly brand: string,
         items: string[]
     ) {
         super(items);
